b67ae4c4a81d0a19e3d518c5a7790ea4b9c9dd64
digiexpouae <tech.digiexpo@gmail.com>
2026-05-09 13:37:05 +0500

    Fix:products

--- src/routes/product.js
+++ src/routes/product.js
@@ -7,7 +7,7 @@ import { singleUpload ,excelUpload, cloudUpload} from "../middleware/multer.js";
 
 const app = express.Router()
 app.get("/search", searchProducts)
-app.post("/new", cloudUpload,authMiddleware, createProduct)
+app.post("/new", cloudUpload,authAdminMiddleware, createProduct)
 app.post("/bulkCreate",excelUpload,authMiddleware,bulkCreateFromExcel)
 app.get("/latest", getLatestProducts)
 app.get("/all", getAllProducts)
